8c958f6853d3e99b2cb08274a4f7f340596c0219
Keith <keithbeste@northwwesternmutual.com>
2017-06-07 11:49:37 -0500

    successful api call - no data displayed yet

--- src/App.js
+++ src/App.js
@@ -3,24 +3,45 @@ import UserForm from './UserForm';
 import RepoList from './RepoList';
 import RepoDetails from './RepoDetails';
 import styles from './style.css';
-
+import axios from 'axios';
 
 class App extends Component {
-  render() {
-    return (
-        <div className="App Container">
-          <h1>Github viewer!</h1>
+        state = {
+        users: []
+    }
 
-          <UserForm />
-          <RepoList />
-          <RepoDetails />
-          
-        </div>
+    handleSearchFormSubmit = (searchTerm) =>{   //function to handle the search within userform
+
+         return axios.get(`https://api.github.com/search/users?q=${searchTerm}`)  //API call
+            .then(({ data }) => {
+                const { items: users } = data;
+                if (users < 1) {
+                    this.setState({
+                        users: []
+                    })
+                    throw new Error('No results');
+                }
+                this.setState({
+                    users: users
+                });
+            });
+    }
+
+    render() {
+      return (
+          <div className="App Container">
+            <h1>Github viewer!</h1>
+
+            <UserForm onSubmit={this.handleSearchFormSubmit} />
+            <RepoList />
+            <RepoDetails />
+            
+          </div>
 
 
 
-    );
-  }
+      );
+    }
 }
 
 export default App;
--- src/UserForm.js
+++ src/UserForm.js
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './style.css';
+import { func } from 'prop-types';
+
+class UserForm extends Component {
+    state = {
+        searchTerm: '',
+        error: null
+    }
+    handleChange = (event) => {
+        this.setState({
+            searchTerm: event.target.value
+        });
+    }
+    handleSubmit = (event) => {
+        this.props.onSubmit(this.state.searchTerm)
+            .then(() => {
+                this.setState({ searchTerm: '', error: null })
+            })
+            .catch(() => {
+                this.setState({
+                    error: 'Unrecognized username'
+                })
+            })
+    }
+    render() {
+        let potentialError = null;
+        if (this.state.error) {
+            potentialError = <p className="notification-box alert">{this.state.error}</p>
+        }
 
 
-
-const UserForm = () => {
     return (
         <div>
             <div className='userFormDetails'>
+            	{potentialError}
                 <label>Search Repositories by Username</label>
-                <input className='inputUserName' type="text" placeholder="username" />
-                <button disabled = 'xyz'>Search</button>
+                <input className='inputUserName' type="text" placeholder="username" value={this.state.searchTerm} onChange={this.handleChange}/>
+                <button onClick={this.handleSubmit} disabled={this.state.searchTerm === ''}>Search</button>    
             </div>
         </div>
     )
 }
+}
+
+UserForm.propTypes = {
+    onSubmit: func.isRequired
+}
 export default UserForm;
+
+
 
 
